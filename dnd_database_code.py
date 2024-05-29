fb5926eddd00d9fb0a8505be38a1424ba4a233be
George <22180@burnside.school.nz>
2024-05-29 20:20:07 +1200

    fixing up layout template

--- dnd_database_code.py
+++ dnd_database_code.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 DATABASE = 'dndspells.db'
 
 app = Flask(__name__)
+
 @app.route('/')
 def spell_names():
     db = sqlite3.connect(DATABASE)
@@ -11,6 +12,10 @@ def spell_names():
     cursor.execute('SELECT spell_name FROM spell ORDER BY spell_level ASC')
     ret = [i[0] for i in cursor.fetchall()]
     return render_template('spell_names_index.html', spells=ret)
+
+@app.route('/home', methods=["GET"])
+def home():
+    return render_template('home_index.html')
 
 
 if __name__ == '__main__':
